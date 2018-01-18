aa8f741b523b784efa04065a999c5b629c2f9960
XiaoYao <yuki.yao@hylinkad.com>
2018-01-19 00:36:44 +0800

    第一次上传

---
+++ app/js/home/index.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div>
+        <slider/>
+
+    </div>
+</template>
+
+<script>
+import Slider from "./slider.vue"
+export default {
+    components: {
+        Slider,
+    },
+}
+</script>
+
+<style lang="scss" module>
+
+</style>
---
+++ app/js/home/slider.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+    <div class="">
+        <Slider :items="items" :cname="$style.slider"/>
+    </div>
+</template>
+
+<script>
+import Slider from "../core/slider.vue"
+export default {
+    components: {
+        Slider,
+    },
+    data() {
+        return {
+            items: [{
+                src: "//chengxinmobile.saic-gm.com/img/kv/kv6.jpg",
+                href: "//chengxinmobile.saic-gm.com/h5/2017xz/",
+            }, {
+                src: "//chengxinmobile.saic-gm.com/img/kv/kv2.jpg",
+                href: "//chengxinmobile.saic-gm.com/h5/2017xz/1",
+            }, {
+                src: "//chengxinmobile.saic-gm.com/img/kv/kv3.jpg",
+                href: "//chengxinmobile.saic-gm.com/h5/2017xz/2",
+            }, {
+                src: "//chengxinmobile.saic-gm.com/img/kv/kv4.jpg",
+                href: "//chengxinmobile.saic-gm.com/h5/2017xz/3",
+            }],
+        }
+    },
+}
+</script>
+
+<style lang="scss" module>
+.slider{
+  margin-top: 100px;
+  img{
+    width: 100%;
+    display:block;
+  }
+}
+</style>
